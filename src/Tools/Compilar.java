1387744d42d95f71d7c1e6099606449b6d475afe
Alejandro Báez <baezdalex@gmail.com>
2015-07-02 10:55:21 -0500

    Clase Comilar

--- src/Tools/Compilar.java
+++ src/Tools/Compilar.java
@@ -5,6 +5,8 @@
  */
 package Tools;
 
+import java.io.File;
+
 /**
  *
  * @author Alejandro Báez
@@ -25,6 +27,7 @@ public class Compilar {
          
           p=obj.exec("cmd.exe /k compilar.bat "+file +" "+ dir);
           //p=obj.exec("cmd.exe /c cd \""+Destino+"\"  & start cmd.exe /k \"xc8 --chip=16F887 mapic.c & exit");
+           p=obj.exec("xc8 --chip=16F887 mapic.c",null,new File(file));
      }
      catch(Exception x)
         {System.out. println("En compilacion "+ x);
